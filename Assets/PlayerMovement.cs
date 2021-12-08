09f18e5319917cfeaa4667ab87ae2470b78faeb8
Peter <201508876PMH@gmail.com>
2021-12-08 21:43:35 +0100

    fixed bug, moved objects

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Vector2 = UnityEngine.Vector2;
 
+
 public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed;
@@ -11,6 +12,22 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     public LayerMask groundLayers;
     float mx;
+
+    public AudioClip jumpAudio;
+    public AudioClip respawnAudio;
+    public AudioClip ouchAudio;
+
+    SpriteRenderer spriteRenderer;
+    internal Animator animator;
+    /*internal new*/ public AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
 
     private void Update(){
         mx = Input.GetAxisRaw("Horizontal");
